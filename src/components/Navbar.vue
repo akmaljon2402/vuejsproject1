f3e13dc11e73e08cf6f177f468be8aa88193b076
akmaljon2402 <polat240298alemdar@gmail.com>
2023-03-01 08:34:41 +0500

    40 dars navigation

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,20 +3,32 @@
       <a class="d-flex align-items-center text-dark text-decoration-none">
         <img :src="logo" alt="logo" style="width: 50px;cursor:pointer;" @click="toHomeHandler"/>
       </a>
-
+      
       <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
-        <RouterLink :to="{name:'login'}" class="me-3 py-2 text-dark text-decoration-none">Login</RouterLink>
-        <RouterLink :to="{name:'register'}" class="me-3 py-2 text-dark text-decoration-none">Register</RouterLink>
+        <template v-if="isLoggedIn">
+          <RouterLink :to="{name:'home'}" class="me-3 py-2 text-dark text-decoration-none">{{ user.username }}</RouterLink>
+        </template>
+        <template v-if="!isLoggedIn">
+          <RouterLink :to="{name:'login'}" class="me-3 py-2 text-dark text-decoration-none">Login</RouterLink>
+          <RouterLink :to="{name:'register'}" class="me-3 py-2 text-dark text-decoration-none">Register</RouterLink>
+        </template>
       </nav>
     </div>
 </template>
 <script>
+import { mapState } from 'vuex';
 import { logo } from '../constants';
 export default {
     data(){
         return {
             logo,
         }
+    },
+    computed:{
+      ...mapState({
+        user: state => state.auth.user,
+        isLoggedIn: state => state.auth.isLoggedIn
+      }),
     },
     methods:{
       toHomeHandler(){
